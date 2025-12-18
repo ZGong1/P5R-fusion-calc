5f244dae53948efc286aca2da57319168bd26898
Eric Peterson <eaptrsn@gmail.com>
2025-12-18 13:39:39 -0600

    <Fusions/> now can print out an ugly list of direct fusions

--- p5r-fusion-calculator/src/components/Fusions.jsx
+++ p5r-fusion-calculator/src/components/Fusions.jsx
@@ -1,16 +1,37 @@
 import CompendiumSelector from './CompendiumSelector'
+import FusionCalculator from '../fusion-calculator-core/FusionCalculator'
+import { customPersonaList, customPersonaeByArcana } from '../fusion-calculator-core/DataUtil';
 import './Fusions.css'
 
 
 function Fusions({ selectedFusion, setSelectedFusion }) {
 
+  const calculator = new FusionCalculator(customPersonaeByArcana)
+
+  const targetWithInfo = selectedFusion 
+    ? customPersonaList.find( persona => persona.name === selectedFusion ) 
+    : null
+
+  const gottenRecipes = selectedFusion
+    ? calculator.getRecipes(targetWithInfo)
+    : null
+
+
   return (
-    <>
+    <div className='fusion-calculator'>
       Please select which Persona you would like to fuse:
       <CompendiumSelector selectedPersona={selectedFusion} setSelectedPersona={setSelectedFusion}/> <br/>
 
-      You have selected {selectedFusion} for a fusion calculation
-    </>
+      {/* {console.log("gottenRecipes: ", gottenRecipes)} */}
+      {gottenRecipes?.map( (recipe, index) => (<div key={index}>
+
+        Recipe {index}:
+        {recipe.sources[0].name} + {recipe.sources[1].name}
+
+      </div>))}
+
+
+    </div>
   )
 
 }
