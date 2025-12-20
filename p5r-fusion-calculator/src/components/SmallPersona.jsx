9d1b68ac705c655f7bd9e16b166715db25557c5d
Eric Peterson <eaptrsn@gmail.com>
2025-12-20 10:46:00 -0600

    Make it visible  that personas are rare or DLC on fusions page

--- p5r-fusion-calculator/src/components/SmallPersona.jsx
+++ p5r-fusion-calculator/src/components/SmallPersona.jsx
@@ -1,11 +1,17 @@
 import './SmallPersona.css'
+import {personaMap} from "../fusion-calculator-core/FusionCalculator.js";
 
 function SmallPersona( { name, personas, fusableImmediate, onClick } ) {
+
+    const personaWithInfo = personaMap[name]
 
     // console.log("personas: ", personas)
     const isFound = personas?.find( persona => persona.name === name )
     const isFusable = fusableImmediate?.find( persona => persona === name)
+    const isRare = personaWithInfo.rare
+    const isDLC = personaWithInfo.dlc
 
+    // compute class name for colorful results
     let className = "small-persona"
     if (isFound) {
         className += " green"
@@ -14,8 +20,18 @@ function SmallPersona( { name, personas, fusableImmediate, onClick } ) {
         className += " fusable"
     }
 
+    // Compute title for tooltips involving DLC/rare personas
+    let title = ""
+    if (isRare) {
+        title = "This is a treasure demon and may have inconsistent fusion results"
+    }
+    if (isDLC) {
+        title = "This is a DLC Persona"
+    }
+
     return (
-        <div className={className} key={name} onClick={onClick}>
+        <div className={className} key={name} onClick={onClick} title={title}>
+            { (isRare || isDLC) && "⚠️"}
             {name}
         </div>
     )
