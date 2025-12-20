176c338311e29f73c7bddb11efa5a4b310d3113a
Eric Peterson <eaptrsn@gmail.com>
2025-12-20 01:01:58 -0600

    Add disclaimer that you already own this a persona when it is `selectedFusion`

--- p5r-fusion-calculator/src/components/Fusions.jsx
+++ p5r-fusion-calculator/src/components/Fusions.jsx
@@ -7,11 +7,16 @@ import SmallPersona from './SmallPersona';
 
 function Fusions({ selectedFusion, setSelectedFusion, personas, fusableImmediate }) {
 
+  // initialize fusion calculator engine for fusion calculations
   const calculator = new FusionCalculator(customPersonaeByArcana)
 
+  // only persona name is passed; get full object with all persona data
   const targetWithInfo = selectedFusion 
     ? customPersonaList.find( persona => persona.name === selectedFusion ) 
     : null
+
+  // TODO: pass into SmallPersona because it is useful here as well
+  const isFound = personas?.find( persona => persona.name === selectedFusion )
 
   const gottenRecipes = selectedFusion
     ? calculator.getRecipes(targetWithInfo)
@@ -25,9 +30,10 @@ function Fusions({ selectedFusion, setSelectedFusion, personas, fusableImmediate
 
       {/* {console.log("gottenRecipes: ", gottenRecipes)} */}
       {/* {gottenRecipes ? gottenRecipes?.length + " recipes were found" : ""} */}
-      
 
-      {/* Loop through 1 level fusions for selectedFusion */}
+      {isFound && `You already own ${selectedFusion}`}
+
+      {/* Loop through list of recipes for `selectedFusion` */}
       {gottenRecipes?.map( (recipe, index) => (<div key={index} className='recipe-wrapper'>
 
         {/* Map instead of just show 2 to handle special fusions that involve more than 2 soners */}
